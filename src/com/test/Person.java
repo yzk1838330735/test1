8e5af245af799eca9e4ba1d1241583ece2a9d01f
yzk <1838330735@qq.com>
2022-05-29 14:01:58 +0800

    get.set

--- src/com/test/Person.java
+++ src/com/test/Person.java
@@ -4,4 +4,37 @@ public class Person {
     private Integer id;
     private String name;
     private Integer age;
+    private String count;
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
